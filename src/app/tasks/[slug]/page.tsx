379c9b8624cf993c1f576209c352c26ddfd3122c
Govardhan Damam <govardhandamam@Govardhans-MacBook-Pro-14.local>
2025-05-01 02:05:43 +0530

    Resolved all console errors/warnings

--- src/app/tasks/[slug]/page.tsx
+++ src/app/tasks/[slug]/page.tsx
@@ -1,18 +1,39 @@
 "use client";
-import React from "react";
+import React, { use } from "react";
 import { useLazyLoadQuery } from "react-relay";
 import { taskByIdQuery } from "@/queries/taskByIdQuery";
 import { type taskByIdQuery as TaskByIdQueryType } from "@/__generated__/taskByIdQuery.graphql";
 import { TaskComponent } from "@/components/Task";
+import { Suspense } from "react";
 
-export default function TasksPage({ params }: { params: { slug: string } }) {
-  const data = useLazyLoadQuery<TaskByIdQueryType>(taskByIdQuery, {
-    id: params.slug,
-  });
+const Loading = () => {
+  return <div>Loading...</div>;
+};
+
+function TaskContent({ slug }: { slug: string }) {
+  const data = useLazyLoadQuery<TaskByIdQueryType>(
+    taskByIdQuery,
+    { id: slug },
+    { fetchPolicy: "store-or-network" }
+  );
 
   if (!data?.task) {
-    return <div>Loading task...</div>;
+    return <Loading />;
   }
 
   return <TaskComponent taskRef={data.task} />;
+}
+
+export default function TasksPage({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = use(params);
+
+  return (
+    <Suspense fallback={<Loading />}>
+      <TaskContent slug={slug} />
+    </Suspense>
+  );
 }
